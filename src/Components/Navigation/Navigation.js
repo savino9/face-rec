36ce26c26f9a2c02860cbb5f2f094b5af7f8e7ae
Savino Rasola <savinorasola90@gmail.com>
2018-09-21 21:12:38 +0200

    first upload project

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<nav style={{ display:'flex', justifyContent: 'flex-end' }}>
+		<p class='f3 link dim black undeline pa3 pointer'>Sign Out</p>
+		</nav>
+	);
+}
+
+export default Navigation;
